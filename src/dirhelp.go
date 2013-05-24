4e5e2e361ebfe41190f6bb57a805158e14810c51
Jon Renner <rennerjc@gmail.com>
2013-05-24 13:40:19 +0800

    switched from regex to map

--- src/dirhelp.go
+++ src/dirhelp.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+	"dirinfo"
 	"flag"
 	"fmt"
 	"os"
 	"os/user"
 	"regexp"
-	"dirinfo"
-	"strings"
 	"runtime"
+	"strings"
 )
 
 var (
 	USER_HOME_DIR string
-	CURRENT_DIR string
-	INFO_SOURCES = "Information sources:\n" +
+	CURRENT_DIR   string
+	INFO_SOURCES  = "Information sources:\n" +
 		"    http://en.wikipedia.org/wiki/Filesystem_Hierarchy_Standard\n" +
 		"    contents of 'man hier'"
-	VERSION       = "1.2"
+	VERSION       = "1.3"
 	flagAllHelp   = flag.Bool("a", false, "print info for all directories")
 	flagVersion   = flag.Bool("v", false, "show version number")
 	flagPrintHelp = flag.Bool("h", false, "print usage info")
@@ -34,7 +34,7 @@ func init() {
 	// because of cross-compiling disabling cgo, this feature is not available
 	// a solution is to compile on the respective platforms
 	// a bit of a pain for such a small feature, though
-	if (runtime.GOARCH == "amd64") {
+	if runtime.GOARCH == "amd64" {
 		currentUser, err := user.Current()
 		handleFatalError(err)
 		USER_HOME_DIR = currentUser.HomeDir
@@ -45,7 +45,7 @@ func init() {
 }
 
 func handleFatalError(err error) {
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
@@ -74,37 +74,36 @@ func formatLookupDir(lookupDir *string) {
 		tempDir = tempDir[:len(tempDir)-1]
 	}
 	for strings.Contains(tempDir, "//") {
-		tempDir = strings.Replace(tempDir, "//", "/", -1);
+		tempDir = strings.Replace(tempDir, "//", "/", -1)
 	}
 	*lookupDir = tempDir
 }
 
-func isDir(pth string) (bool, error) {
-	fi, err := os.Stat(pth)
+func isDir(path string) (bool, error) {
+	fi, err := os.Stat(path)
 	if err != nil {
 		return false, err
 	}
 	return fi.Mode().IsDir(), nil
 }
 
-func isADirectory(path string) bool {
-	f, err := os.Open(path)
-	if (err != nil) {
-		fmt.Println("ERROR: " + err.Error())
-		return false
+func lookupDirInfo(lookupDir string) string {
+	formatLookupDir(&lookupDir)
+	info := ""
+	if lookupDir == USER_HOME_DIR {
+		info = fmt.Sprintf("[%s] %s\n", USER_HOME_DIR, dirinfo.HOME_DIR_INFO)
+	} else {
+		result := dirinfo.Directories[lookupDir]
+		if (result != "") {
+			info = fmt.Sprintf("[%s] %s\n", lookupDir, result)
+		}
 	}
-	stat, err := f.Stat()
-	if (err != nil) {
-		fmt.Println("ERROR: " + err.Error())
-		return false;
-	}
-	return stat.IsDir()
+	return info
 }
 
 func printDirInfo(lookupDirList []string) {
-	var foundInfo bool = false;
+	foundAtLeastOne := false
 	didNotFind := make([]string, 0)
-	results := testRE.FindAllStringSubmatch(string(dirinfo.FHS_INFO), -1)
 	infoToPrint := ""
 	for _, lookupDir := range lookupDirList {
 		isDir, err := isDir(lookupDir)
@@ -112,29 +111,20 @@ func printDirInfo(lookupDirList []string) {
 		if !isDir {
 			continue
 		}
-		foundInfo = false
-		if lookupDir == USER_HOME_DIR || *flagAllHelp {
-			infoToPrint += fmt.Sprintf("[%v] %v\n", USER_HOME_DIR, dirinfo.HOME_DIR_INFO)
-			foundInfo = true;
-		}
-		formatLookupDir(&lookupDir)
-		for _, regexResult := range results {
-			dir := regexResult[1]
-			help := regexResult[2]
-			if  dir == lookupDir || *flagAllHelp {
-				infoToPrint += fmt.Sprintf("[%v] %v\n", dir, help)
-				foundInfo = true
-			}
-		}
-		if (!foundInfo) {
+		info := lookupDirInfo(lookupDir)
+		if (info != "") {
+			foundAtLeastOne = true
+			infoToPrint += info
+		} else {
 			didNotFind = append(didNotFind, lookupDir)
 		}
 	}
-	if (len(didNotFind) > 0) {
-		fmt.Println("Did not find information for the following directories:")
+	if len(didNotFind) > 0 {
 		for _, dir := range didNotFind {
-			fmt.Println(dir)
+			fmt.Printf("'%s' - no information found\n", dir)
 		}
+	}
+	if !foundAtLeastOne {
 		printUsage()
 	}
 	fmt.Print(infoToPrint)
@@ -150,9 +140,17 @@ func main() {
 		fmt.Println(INFO_SOURCES)
 		os.Exit(0)
 	}
+	if *flagAllHelp {
+		allInfo := "Information on all directories available:\n"
+		for dir, info := range dirinfo.Directories {
+			allInfo += fmt.Sprintf("[%s] %s\n", dir, info)
+		}
+		fmt.Print(allInfo)
+	    os.Exit(0)
+	}
 	lookupDirList := flag.Args()
 	for i, dir := range lookupDirList {
-		if (lookupDirList[i][0] != '/') {
+		if lookupDirList[i][0] != '/' {
 			lookupDirList[i] = CURRENT_DIR + "/" + dir
 		}
 	}
